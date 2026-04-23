565286e95c73bb0658cd994d3ca9610cee22a0d4
dipayan-pal <103555122+dipayan-pal@users.noreply.github.com>
2026-04-23 05:52:30 +0000

    Added the fastapi code to serve the model

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import joblib
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+## Load the model and scaler
+classifier = joblib.load('model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+## Define the shape of the incoming data
+class InputData(BaseModel):
+    age: int
+    estimated_salary: int
+
+@app.post('/predict')
+def predict(data: InputData):
+    input_data = np.array([[data.age, data.estimated_salary]])
+    input_data_scaled = scaler.transform(input_data)
+    prediction = classifier.predict(input_data_scaled)
+    return {'prediction': int(prediction[0])}
